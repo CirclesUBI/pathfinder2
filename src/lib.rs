917ad559e0ff89caef20bdb5c4d50e16eca6cb40
chriseth <chris@ethereum.org>
2022-09-13 14:03:20 +0200

    Test

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod flow;
+pub mod io;
+pub mod types;
---
+++ tests/integration.rs
@@ -0,0 +1,14 @@
+use pathfinder2::flow::compute_flow;
+use pathfinder2::io::read_edges_binary;
+use pathfinder2::types::Address;
+
+#[test]
+fn test_flow() {
+    let edges = read_edges_binary(&"edges.dat".to_string()).unwrap();
+    let transfers = compute_flow(
+        &Address::from("0x8DC7e86fF693e9032A0F41711b5581a04b26Be2E"),
+        &Address::from("0x42cEDde51198D1773590311E2A340DC06B24cB37"),
+        &edges,
+    );
+    println!("{:?}", transfers);
+}
