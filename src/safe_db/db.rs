5969204a1fd9dd83feb0f05a2bf6b84c2f97f8f0
chriseth <chris@ethereum.org>
2022-12-12 23:03:57 +0100

    Compute edges from safes.

--- src/safe_db/db.rs
+++ src/safe_db/db.rs
@@ -1,16 +1,60 @@
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, default};
 
-use crate::types::{Address, Safe};
+use crate::types::{edge::EdgeDB, Address, Edge, Safe, U256};
 
 #[derive(Default, Debug)]
 pub struct DB {
     safes: BTreeMap<Address, Safe>,
     token_owner: BTreeMap<Address, Address>,
+    edges: EdgeDB,
 }
 
 impl DB {
     pub fn new(safes: BTreeMap<Address, Safe>, token_owner: BTreeMap<Address, Address>) -> DB {
         println!("{} safes, {} tokens", safes.len(), token_owner.len());
-        DB { safes, token_owner }
+        let mut db = DB {
+            safes,
+            token_owner,
+            ..Default::default()
+        };
+        db.compute_edges();
+        db
+    }
+
+    fn compute_edges(&mut self) {
+        let mut edges = vec![];
+        for (user, safe) in &self.safes {
+            // trust connections
+            for (send_to, percentage) in &safe.limit_percentage {
+                if *user == *send_to {
+                    continue;
+                }
+                if let Some(receiver_safe) = self.safes.get(send_to) {
+                    let limit = safe.trust_transfer_limit(receiver_safe, *percentage);
+                    if limit != U256::from(0) {
+                        edges.push(Edge {
+                            from: *user,
+                            to: *send_to,
+                            token: *user,
+                            capacity: limit,
+                        })
+                    }
+                }
+            }
+            // send tokens back to owner
+            for (token, balance) in &safe.balances {
+                if let Some(owner) = self.token_owner.get(token) {
+                    if *user != *owner {
+                        edges.push(Edge {
+                            from: *user,
+                            to: *owner,
+                            token: *owner,
+                            capacity: *balance,
+                        })
+                    }
+                }
+            }
+        }
+        self.edges = EdgeDB::new(edges)
     }
 }
--- src/types/safe.rs
+++ src/types/safe.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::{cmp::min, collections::BTreeMap};
 
 use super::{Address, U256};
 
@@ -9,4 +9,27 @@ pub struct Safe {
     /// Limit percentage in "send to" direction
     pub limit_percentage: BTreeMap<Address, u8>,
     pub organization: bool,
+}
+
+impl Safe {
+    pub fn balance(&self, token: &Address) -> U256 {
+        *self.balances.get(token).unwrap_or(&U256::from(0))
+    }
+    /// @returns how much of their own tokens a user can send to receiver.
+    pub fn trust_transfer_limit(&self, receiver: &Safe, trust_percentage: u8) -> U256 {
+        if receiver.organization {
+            // TODO treat this as "return to owner"
+            self.balance(&self.token_address)
+        } else {
+            let receiver_balance = receiver.balance(&self.token_address);
+            let amount = (receiver.balance(&receiver.token_address)
+                * U256::from(trust_percentage as u128))
+                / U256::from(100);
+            if amount < receiver_balance {
+                U256::from(0)
+            } else {
+                min(amount - receiver_balance, self.balance(&self.token_address))
+            }
+        }
+    }
 }
