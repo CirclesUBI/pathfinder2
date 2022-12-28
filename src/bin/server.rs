65ed77d442e7955e64e84410d0db0e4ccdb4eab5
chriseth <chris@ethereum.org>
2022-12-29 00:06:06 +0100

    Fix clippy.

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -3,6 +3,8 @@ use std::env;
 use pathfinder2::server;
 
 fn main() {
-    let listen_at = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
+    let listen_at = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
     server::start_server(&listen_at, 10, 4);
 }
