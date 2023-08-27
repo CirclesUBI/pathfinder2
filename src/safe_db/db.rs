c94dbc8600816f5e3776f0020faf5931dc8982f1
ele <lluna@bitspossessed.org>
2023-08-28 00:15:59 +0200

    Remove repeated comments

--- src/safe_db/db.rs
+++ src/safe_db/db.rs
@@ -29,19 +29,6 @@ impl DB {
         &self.edges
     }
 
-    /// This method calculates how much of a token a user can send to a given receiver.
-    /// The transfer limit is based on the trust relationship between the user and the receiver,
-    /// denoted by the trust_percentage argument.
-    /// If the receiver is an organization, the method simply returns the balance of the sender, i.e.,
-    /// the user can send all their tokens to an organization. Likewise, if the receiver is the owner of
-    /// the token, the returned value is the balance of the sender of that token.
-    /// If the receiver is a regular user which is not the owner of the token, the method calculates the
-    /// transfer limit based on the balance of the receiver and the trust percentage. The method scales
-    /// down the receiver's balance based on the trust percentage and compares it with the original
-    /// balance to calculate the maximum amount the user can send.
-    /// The maximum amount a user can send is the smaller of the user's balance of the token and the
-    /// difference between the scaled receiver's balance and the balance calculated based on the trust
-    /// percentage.
     fn compute_edges(&mut self) {
         // Universal computation of edges
         // Let's assume that any "token" is represented by the address of its owner in the edges
